4deb983a3dd3127ba58abca2b91f340d8677b3ba
alysson.morais <alysson.morais@dataprev.gov.br>
2025-08-24 19:34:31 -0300

    first commit

---
+++ src/main/java/br/com/erudito/controllers/GreetingController.java
@@ -0,0 +1,22 @@
+package br.com.erudito.controllers;
+
+import br.com.erudito.modelo.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counterId = new AtomicLong();
+
+    //http://localhost:8080/greeting?name=Alsson
+    @RequestMapping("/greeting")
+
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counterId.incrementAndGet(), String.format(template, name));
+    }
+}
---
+++ src/main/java/br/com/erudito/modelo/Greeting.java
@@ -0,0 +1,4 @@
+package br.com.erudito.modelo;
+
+public record Greeting(long id, String content) {
+}
